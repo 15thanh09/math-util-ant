67fbdfb82220502138562571679f66c520fb261d
15thanh09 <15thanh09@gmail.com>
2022-10-14 10:45:55 +0700

    update getF() to see green again

--- src/com/thanhdao/mathutil/core/MathUtil.java
+++ src/com/thanhdao/mathutil/core/MathUtil.java
@@ -50,11 +50,12 @@ public class MathUtil {
     
     */
     public static long getFactorial(int n){
-//        if (n < 0 || n > 20){
-//            throw new IllegalArgumentException("Invalid n. N must be between 0...20");
-//        }
+        if (n < 0 || n > 20){
+            throw new IllegalArgumentException("Invalid n. N must be between 0...20");
+        }
         if (n == 0 || n == 1)
-            return 1; // CHỐT DỪNG ĐỆ QUY 
+            return 1; 
+        // CHỐT DỪNG ĐỆ QUY 
         // TỪ 2 GIAI THỪA LÀ ĐOẠN CODE DƯỚI ĐÂY
         return  n* getFactorial(n -1); 
         
